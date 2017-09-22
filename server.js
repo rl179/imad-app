ddcaf6e7a7e6cace93ea2dcdbae206781a9d3ba5
rl179 <31285885+rl179@users.noreply.github.com>
2017-09-22 21:17:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});  
+
+app.get('/Article-one', function (req, res) {
+  res.send('Article one is severed here.');
+});
+
+app.get('/Article-two', function (req, res) {
+  res.send('Article two is severed here.');
+});
+
+app.get('/Article-three', function (req, res) {
+  res.send('Article three is severed here.');
 });
 
 app.get('/ui/style.css', function (req, res) {
